49606c7928af937d5b7668f6b0750cb703ee2688
Evan Candler <emcandler21@gmail.com>
2018-03-13 23:10:51 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,8 @@ module Players
     if board.cells.count{|cell| cell != " "} == 0
       "1"
     elsif board.cells.count{|cell| cell != " "} == 1
+      board.cells.detect("X") == 4 ? "5" : "1"
+
       possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       valid_moves = []
       possible_moves.each do |move| #iterate over the array of valid moves
